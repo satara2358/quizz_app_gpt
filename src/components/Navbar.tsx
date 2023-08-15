ce68c48e64f8511b971426b243a3b86a26e1897c
satara <25549072+satara2358@users.noreply.github.com>
2023-08-15 00:14:27 -0500

    updateError

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,33 @@
-import { getAuthSession } from '@/lib/nextauth'
-import React from 'react'
-import Link  from 'next/link'
-import SingInButton from './SingInButton'
+import Link from "next/link";
+import React from "react";
 
-type Props = {}
+import UserAccountNav from "./UserAccountNav";
+// import { ThemeToggle } from "./ThemeToggle";
+import { getAuthSession } from "@/lib/nextauth";
+import SignInButton from "./SingInButton";
 
-const Navbar = async (props: Props) => {
-  return(
-    <div className='fixed inset-x-0 top-0 bg-white dark:bg-gray-950 h-fit border-b border-zinc-300 py-2'>
-      <div className='flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl'>
-        <Link href="/" className='flex items-center gap-2'>
-          <p className='rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white'>
-            QuizGoogle
+const Navbar = async () => {
+  const session = await getAuthSession();
+  return (
+    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300  py-2 ">
+      <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
+        {/* Logo */}
+        <Link href={"/"} className="flex items-center gap-2">
+          <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
+            Quizmify
           </p>
         </Link>
-        <div className='flex items-center'>
-          <SingInButton text='Sign In'/>
+        <div className="flex items-center">
+          {/* <ThemeToggle className="mr-4" /> */}
+          {session?.user ? (
+            <UserAccountNav user={session.user} />
+          ) : (
+            <SignInButton text={"Sign In"} />
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -1,20 +1,31 @@
-import { DefaultSession, NextAuthOptions, getServerSession } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "./db";
-import  GoogleProvider  from "next-auth/providers/google";
-import { getServers } from "dns";
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type NextAuthOptions,
+  type DefaultSession,
+} from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/lib/db";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
-      id: String
-    } & DefaultSession["user"]
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
   }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: string
+    id: string;
   }
 }
 
@@ -22,12 +33,13 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     jwt: async ({ token }) => {
       const db_user = await prisma.user.findFirst({
         where: {
-          email: token?.email
-        }
+          email: token?.email,
+        },
       });
       if (db_user) {
         token.id = db_user.id;
@@ -35,25 +47,24 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
     session: ({ session, token }) => {
-      if (token?.id) {
+      if (token) {
         session.user.id = token.id;
         session.user.name = token.name;
         session.user.email = token.email;
         session.user.image = token.picture;
       }
       return session;
-    }
+    },
   },
-  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
-  ]
-}
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
 
 export const getAuthSession = () => {
   return getServerSession(authOptions);
-}
+};
