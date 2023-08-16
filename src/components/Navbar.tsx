e0af7a4c78f2495be5b2c9446fc4e9dfb9040601
satara <25549072+satara2358@users.noreply.github.com>
2023-08-15 22:36:00 -0500

    UpdateError

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { use } from "react";
 
 import UserAccountNav from "./UserAccountNav";
 // import { ThemeToggle } from "./ThemeToggle";
@@ -8,6 +8,7 @@ import SignInButton from "./SingInButton";
 
 const Navbar = async () => {
   const session = await getAuthSession();
+  console.log(session?.user);
   return (
     <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300  py-2 ">
       <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
